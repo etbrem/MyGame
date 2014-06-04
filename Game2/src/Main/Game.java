72953c424396b38659b9a281d55f1ea38384ce49
etbrem <etbrem2@gmail.com>
2014-06-04 17:22:14 +0300

    Update Game.java

--- Game2/src/Main/Game.java
+++ Game2/src/Main/Game.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class Game {
 
+	//Yet again this is a test.
+
 	public static void main(String[] args) {
 		JFrame window = new JFrame("My Game");
 		window.add(new GamePanel());
