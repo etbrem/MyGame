1228f127456165d206d1f3ba021ff8e8723a8ab8
etbrem <etbrem2@gmail.com>
2014-06-04 18:30:31 +0300

    Update Game.java

--- Game2/src/Main/Game.java
+++ Game2/src/Main/Game.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 
 public class Game {
 	
-	//SSH-RSA TEST
-	
 	public static void main(String[] args) {
 		JFrame window = new JFrame("My Game");
 		window.add(new GamePanel());
