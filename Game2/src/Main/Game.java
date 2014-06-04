59966ebbf57a1f934960c44a564517c35a150aa9
Et <Et@BREMPC>
2014-06-04 17:55:30 +0300

    vcds

--- Game2/src/Main/Game.java
+++ Game2/src/Main/Game.java
@@ -3,9 +3,6 @@ package Main;
 import javax.swing.*;
 
 public class Game {
-
-	//test
-
 	public static void main(String[] args) {
 		JFrame window = new JFrame("My Game");
 		window.add(new GamePanel());
